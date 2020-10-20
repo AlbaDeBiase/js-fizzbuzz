86301b516702c1a467a1d9ab9956a237681aae6d
Alba De biase <debiasealba@gmail.com>
2020-10-20 16:50:23 +0200

    soluzione1

--- main.js
+++ main.js
@@ -4,12 +4,17 @@
 // al posto dei multipli sia di 3 che di 5, deve stampare "FizzBuzz"
 
 
-// Stampo i numeri da 1 a 100 
-for (var i = 0; i < 100; i++) {
-    console.log(i+1);
+// Stampo i numeri da 1 a 100
+for (var i = 1; i <= 100; i++) {
 
-var numero =
-if( x % 3 == 0 ) {
-console.log("Multiplo  di 3");
-}
+// // al posto dei multipli di 3, deve stampare "Fizz"
+if ((i % 3 == 0) && (i % 5 == 0)){
+               console.log("FizzBuzz");
+           } else if(i % 3 == 0){
+               console.log("Fizz");
+           } else if(i % 5 == 0){
+               console.log("Buzz");
+           } else {
+               console.log(i);
+           }
 }
